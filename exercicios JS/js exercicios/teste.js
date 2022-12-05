bb7b4d24970316b854233e57810611a8c7080a5e
José Leonardo <leodkvt@gmail.com>
2022-12-05 12:18:03 -0300

    teste

--- exercicios JS/js exercicios/teste.js
+++ exercicios JS/js exercicios/teste.js
@@ -1,5 +1,22 @@
-function areaQuadrado(lado) {
-    return lado * lado;
-}
+var nome = 'Leonardo';
 
-console.log(areaQuadrado(10));
+console.log(nome.length);  //"8" nome.length informa a quantidade de caracteres.
+console.log(nome.charAt(3));  //"n" nome.charAt(3) informa qual caractere esta naquela posição.
+console.log(nome.replace('onardo', 'andro'));  //"Leandro" troca caractere pelo outro.ff
+
+
+// toString : Transformar Array em uma string
+language.toString()
+// join : Transforme Array em 1 string, dando ponto comum
+language.join()
+// pop : Remove o último elemento da matriz e retorna o elemento excluído
+language.pop()
+// push : Adiciona 1 ou mais elementos ao final da matriz e retorna o novo comprimento da matriz
+language.push('NodeJS')
+// shift : Remova o elemento no início da matriz
+language.shift()
+// Adicione 1 ou mais elementos ao início da matriz e retorne o novo comprimento da matriz
+language.unshift('NodeJS')
+
+
+window.navigator.vibrate(500);
